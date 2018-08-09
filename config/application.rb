85b86b19646e96d6e2b8545b756c2103ed01ec28
ryota.suzuki <getty2017@gmail.com>
2018-08-09 10:56:07 +0900

    set preparation

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,11 @@ module TaMicroposts
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Rails.application.config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.skip_routes
+    end
   end
 end

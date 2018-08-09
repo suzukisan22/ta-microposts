4da9af40438f8c28cf2318aaec6d8406434f3a3f
ryota.suzuki <getty2017@gmail.com>
2018-08-09 19:38:21 +0900

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -6,7 +7,14 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
 
   resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
 end
